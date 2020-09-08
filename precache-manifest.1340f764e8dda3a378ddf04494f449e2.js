8aa018260bd34d729efb48110aba89d2c135a974
Vadzim Tahunou <v.tahunou@godeltech.com>
2020-09-08 18:40:26 +0300

    Updates

---
+++ precache-manifest.1340f764e8dda3a378ddf04494f449e2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a152e19e9fb7ec17fa563811edbcf9b",
+    "url": "/VadimTagunov/index.html"
+  },
+  {
+    "revision": "d83754f12fc54d1c8f2c",
+    "url": "/VadimTagunov/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "5c98fe9bbaf0bda0dc02",
+    "url": "/VadimTagunov/static/css/main.f37c6af0.chunk.css"
+  },
+  {
+    "revision": "d83754f12fc54d1c8f2c",
+    "url": "/VadimTagunov/static/js/2.fe55e566.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/VadimTagunov/static/js/2.fe55e566.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c98fe9bbaf0bda0dc02",
+    "url": "/VadimTagunov/static/js/main.a572b003.chunk.js"
+  },
+  {
+    "revision": "ddca95cee83d219c15fa",
+    "url": "/VadimTagunov/static/js/runtime-main.c0e265eb.js"
+  },
+  {
+    "revision": "82e09f7862a1e26fb7dfe1d0f8adc87f",
+    "url": "/VadimTagunov/static/media/SEGA.82e09f78.TTF"
+  }
+]);
